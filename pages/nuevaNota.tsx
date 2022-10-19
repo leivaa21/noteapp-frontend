a4030b79f3434b7674e6a53d4f27e196566062e1
JJtrenado <juanjotrenadozafra@protonmail.com>
2022-10-19 22:04:21 +0200

    estilos, form and buttons

--- pages/nuevaNota.tsx
+++ pages/nuevaNota.tsx
@@ -3,24 +3,20 @@ import Image from 'next/image'
 import Link from "next/link"; // esto hace que no tenga que recargar toda la pagina cuando cambias de página
 import React from 'react';
 import Form from '../components/Form';
-import Notas from '../components/Notas';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
-    <div className={styles.cuerpo}>  
+    <div>  
       <Head>
-        <title>Notas</title>
+        <title>Añadir Nota</title>
         <meta name="description" content="Tu WebApp de notas" />
         <link rel="icon" href="/favicon1.ico" />
       </Head>
-      <div className={styles.container}><h1>Notas</h1></div>
-      <div className={styles.container}><Notas /></div>
-      
-      <Link href="/nuevaNota" passHref>
-        <button className={styles.botonAñadir}>+</button>
-      </Link>
+
+      <Form />
+
     </div>
     </>
     )
